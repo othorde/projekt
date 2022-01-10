fc157d3ae13e8b0da8cf1f34a168ea244f2ff485
Oliver Thordeman <o.thordeman@live.se>
2022-01-10 15:59:46 +0000

    1

--- src/Api.js
+++ src/Api.js
@@ -14,7 +14,6 @@ export async function getAUser(id) {
 
 /* Hämtar alla användare  */
 export async function getAllUsers() {
-    console.log("")
     try {
         const endpoint = `${API_URL}/customers`;
         let res = await (await fetch(endpoint)).json();
